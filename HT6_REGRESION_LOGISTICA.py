aaf26d226ad4852e766835cb319feceb9de7dae5
Raúl Angel Jiménez <46568595+raulangelj@users.noreply.github.com>
2022-04-15 12:25:41 -0600

    Add files via upload

---
+++ HT6_REGRESION_LOGISTICA.py
@@ -0,0 +1,10 @@
+# %% [markdown]
+# # HOJA DE TRABAJO 6 REGRESION LOGISTICA
+
+# Raul Jimenez 19017
+
+# Donaldo Garcia 19683
+
+# Oscar Saravia 19322
+
+# link al repo: https://github.com/raulangelj/HT6_REGRESION_LOGISTICA
