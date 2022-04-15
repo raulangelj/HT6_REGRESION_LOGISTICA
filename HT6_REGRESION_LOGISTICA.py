7a493140e7b6c7b83762564ccba97749a1e8f227
Raúl Angel Jiménez <raulangelj@gmail.com>
2022-04-15 12:48:19 -0600

    first done

--- HT6_REGRESION_LOGISTICA.py
+++ HT6_REGRESION_LOGISTICA.py
@@ -8,3 +8,152 @@
 # Oscar Saravia 19322
 
 # link al repo: https://github.com/raulangelj/HT6_REGRESION_LOGISTICA
+
+# %%
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sb
+import sklearn.cluster as cluster
+import sklearn.metrics as metrics
+import sklearn.preprocessing
+import scipy.cluster.hierarchy as sch
+import seaborn as sns
+import skfuzzy as fuzz
+import pylab
+import sklearn.mixture as mixture
+import random
+import math
+import statsmodels.api as sm
+from sklearn.linear_model import LogisticRegression
+from yellowbrick.cluster import SilhouetteVisualizer
+from yellowbrick.datasets import load_nfl
+from sklearn import linear_model
+from sklearn.metrics import mean_squared_error, r2_score, accuracy_score, precision_score, recall_score, f1_score
+from sklearn import datasets
+from scipy import stats
+from sklearn.metrics import confusion_matrix as Confusion_Matrix
+from sklearn.model_selection import train_test_split
+
+# %matplotlib inline
+from mpl_toolkits.mplot3d import Axes3D
+plt.rcParams['figure.figsize'] = (16, 9)
+plt.style.use('ggplot')
+
+# %% [markdown]
+# ## Use los mismos conjuntos de entrenamiento y prueba que utilizó en las dos hojas anteriores.
+
+# %%
+train = pd.read_csv('./train.csv', encoding='latin1')
+train.head()
+
+# %%
+usefullAttr = ['SalePrice', 'LotArea', 'OverallCond', 'YearBuilt', 'MasVnrArea', 'TotalBsmtSF', '1stFlrSF',
+               '2ndFlrSF', 'GrLivArea', 'TotRmsAbvGrd', 'GarageCars', 'WoodDeckSF', 'OpenPorchSF', 'EnclosedPorch', 'PoolArea', 'Neighborhood', 'OverallQual']
+
+
+# %%
+data = train[usefullAttr]
+data.head()
+
+
+# %%
+sns.pairplot(data[['SalePrice', 'LotArea', 'TotalBsmtSF',
+             'GrLivArea', 'TotRmsAbvGrd', 'OverallQual']], hue='SalePrice')
+plt.show()
+
+# %%
+plt.subplots(figsize=(8, 8))
+sns.heatmap(data[['SalePrice', 'LotArea', 'TotalBsmtSF',
+                  'GrLivArea', 'TotRmsAbvGrd', 'OverallQual']].corr(), annot=True, fmt="f").set_title("Correlación de las variables numéricas de Iris")
+
+
+# %%
+# NORMALIZAMOS DATOS
+if 'Neighborhood' in data.columns:
+    usefullAttr.remove('Neighborhood')
+data = train[usefullAttr]
+X = []
+for column in data.columns:
+    try:
+        column
+        data[column] = (data[column]-data[column].mean()) / \
+            data[column].std()
+        X.append(data[column])
+    except Exception:
+        continue
+data_clean = data.dropna(subset=usefullAttr, inplace=True)
+X_Scale = np.array(data)
+X_Scale
+
+# %%
+kmeans = cluster.KMeans(n_clusters=3)
+kmeans.fit(X_Scale)
+kmeans_result = kmeans.predict(X_Scale)
+kmeans_clusters = np.unique(kmeans_result)
+for kmeans_cluster in kmeans_clusters:
+    # get data points that fall in this cluster
+    index = np.where(kmeans_result == kmeans_cluster)
+    # make the plot
+    plt.scatter(X_Scale[index, 0], X_Scale[index, 1])
+plt.show()
+
+# %%
+data['cluster'] = kmeans.labels_
+print(data[data['cluster'] == 0].describe().transpose())
+print(data[data['cluster'] == 1].describe().transpose())
+print(data[data['cluster'] == 2].describe().transpose())
+# ## Variable clasificacion
+# %%
+# Clasificacion de casas en: Economias, Intermedias o Caras.
+data.fillna(0)
+
+minPrice = data['SalePrice'].min()
+maxPrice = data['SalePrice'].max()
+division = (maxPrice - minPrice) / 3
+data['Clasificacion'] = data['LotArea']
+
+data['Clasificacion'][data['SalePrice'] < minPrice + division] = 'Economica'
+data['Clasificacion'][data['SalePrice'] >= minPrice + division] = 'Intermedia'
+data['Clasificacion'][data['SalePrice'] >= minPrice + division * 2] = 'Caras'
+
+# %% [markdown]
+# #### Contamos la cantidad de casas por clasificacion
+
+# %%
+# Obtener cuantos datos hay por cada clasificacion
+print(data['Clasificacion'].value_counts())
+
+# %% [markdown]
+# ## Dividmos en entrenamiento y prueba
+
+# %% [markdown]
+# # Estableciendo los conjuntos de Entrenamiento y Prueba
+
+# %%
+y = data['Clasificacion']
+X = data[['SalePrice', 'LotArea', 'TotalBsmtSF',
+          'GrLivArea', 'TotRmsAbvGrd', 'OverallQual']]
+
+# %%
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.3, train_size=0.7)
+y_train
+
+# %% [markdown]
+# 70% de entrenamiento y 30% prueba
+
+# %%
+X_train.info()
+
+# %%
+X_test.info()
+
+# [markdown]
+# ## 1. Cree  una  variable  dicotómica  por  cada  una  de  las  categorías de  la  variable  respuesta categórica que creó en hojas anteriores. Debería tener 3 variables dicotómicas (valores 0 y 1) una que diga si la vivienda es cara o no, media o no, económica o no.
+
+# %%
+data["CARA"] = np.where(data["cluster"] == 2, 1, 0)
+data["MEDIA"] = np.where(data["cluster"] == 1, 1, 0)
+data["ECONOMICA"] = np.where(data["cluster"] == 0, 1, 0)
+data
